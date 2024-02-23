8ffbb1a29952d507a54b64d6361a18b08080f07c
Nayeem Ahmad Dar <nayeem.marketinglad@gmail.com>
2024-02-23 16:06:12 +0530

    Now Updated

--- components/CaursalLogos.tsx
+++ components/CaursalLogos.tsx
@@ -4,12 +4,12 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Image from 'next/image';
 
-import img1 from '../public/logos/Apexure (2).webp';
-import img2 from '../public/logos/Beaconstac (1).webp';
-import img3 from '../public/logos/DOOR3 (2).webp';
-import img4 from '../public/logos/Infuse-Media (2).webp';
-import img5 from '../public/logos/LeadGen-App (2).webp';
-import img6 from '../public/logos/Taxmann (2).webp';
+import img1 from '../public/logos/Apexure (1).png';
+import img2 from '../public/logos/Beaconstac.png';
+import img3 from '../public/logos/DOOR3.png';
+import img4 from '../public/logos/Infuse-Media (2).png';
+import img5 from '../public/logos/LeadGen-App.png';
+import img6 from '../public/logos/Taxmann.png';
 import img7 from '../public/logos/Untitled-1-1.png';
 
 const CustomNextArrow = (props) => (

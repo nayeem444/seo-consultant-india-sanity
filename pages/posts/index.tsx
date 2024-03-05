5aaadaca43fb3e0f3dab54f4c51f9c0bd8394ae4
Nayeem Ahmad Dar <nayeem.marketinglad@gmail.com>
2024-03-05 12:20:50 +0530

    updated

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,42 +1,48 @@
-import IndexPage from 'components/IndexPage'
-import PreviewIndexPage from 'components/PreviewIndexPage'
-import { readToken } from 'lib/sanity.api'
-import { getAllPosts, getClient, getSettings } from 'lib/sanity.client'
-import { Post, Settings } from 'lib/sanity.queries'
-import { GetStaticProps } from 'next'
-import type { SharedPageProps } from 'pages/_app'
+import Head from 'next/head';
+import IndexPage from 'components/IndexPage';
+import PreviewIndexPage from 'components/PreviewIndexPage';
+import { readToken } from 'lib/sanity.api';
+import { getAllPosts, getClient, getSettings } from 'lib/sanity.client';
+import { Post, Settings } from 'lib/sanity.queries';
+import { GetStaticProps } from 'next';
+import type { SharedPageProps } from 'pages/_app';
 
 interface PageProps extends SharedPageProps {
-  posts: Post[]
-  settings: Settings
+  posts: Post[];
+  settings: Settings;
 }
 
 interface Query {
-  [key: string]: string
+  [key: string]: string;
 }
 
 export default function Page(props: PageProps) {
-  const { posts, settings, draftMode } = props
- 
+  const { posts, settings, draftMode } = props;
 
   if (draftMode) {
-    return <PreviewIndexPage posts={posts} settings={settings} />
+    return <PreviewIndexPage posts={posts} settings={settings} />;
   }
 
-  return <IndexPage posts={posts} settings={settings} />
+  return (
+    <>
+      <Head>
+        <title>Blogs</title>
+        <meta name="SEO Blogs" content="" />
+      </Head>
+      <IndexPage posts={posts} settings={settings} />
+    </>
+  );
 }
 
 export const getStaticProps: GetStaticProps<PageProps, Query> = async (ctx) => {
-  const { draftMode = false } = ctx
-  const client = getClient(draftMode ? { token: readToken } : undefined)
+  const { draftMode = false } = ctx;
+  const client = getClient(draftMode ? { token: readToken } : undefined);
 
   const [settings, posts = []] = await Promise.all([
     getSettings(client),
     getAllPosts(client),
-  ])
+  ]);
 
-
- 
   return {
     props: {
       posts,
@@ -44,5 +50,5 @@ export const getStaticProps: GetStaticProps<PageProps, Query> = async (ctx) => {
       draftMode,
       token: draftMode ? readToken : '',
     },
-  }
-}
+  };
+};

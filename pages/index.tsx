7d48ae9f7e63eb31203c32576044db55848f6fe0
Nayeem Ahmad Dar <nayeem.marketinglad@gmail.com>
2024-03-05 11:27:43 +0530

    favicon added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import Head from 'next/head';
+import Link from 'next/link'
 import Hero from '../components/HeroSection'
 import Header from '../components/Navbar'
 import About from '../components/About'
@@ -14,11 +15,11 @@ import Content from "../components/ContentSection";
 import Calan from "../components/PopUp";
 
 export default function Home() {
-  return (
+  return ( 
    <>
     <Head>
-    <link rel="icon" href="../public/favicon.ico"/>
-        <title>Shahid Shahmiri - Your Expert SEO Consultant in India</title>
+    <link rel="icon" href="/favicon.ico" sizes="any" />
+            <title>Shahid Shahmiri - Your Expert SEO Consultant in India</title>
         <meta name="description" content=" Looking for the #1 SEO consultant in India? My proven tactics drive traffic and increase revenue. Schedule a Free consultation." />
         
       </Head>

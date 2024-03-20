94359a46c617465349d2571659f0e0bd1146706c
Nayeem Ahmad Dar <nayeem.marketinglad@gmail.com>
2024-03-20 16:38:35 +0530

    updated fix

--- components/ServicesSection.tsx
+++ components/ServicesSection.tsx
@@ -150,7 +150,7 @@ export default Service;
 const ServiceCard = ({ icon, title, details }) => {
   return (
     <>
-      <div className="w-full px-4 md:w-1/2 lg:w-1/3">
+      <div className="w-full h-50 px-4 md:w-1/2 lg:w-1/3">
         <div className="mb-9 rounded-[20px] bg-white p-10 shadow-2 hover:shadow-lg dark:bg-dark-2 md:px-7 xl:px-10">
           <div className="mb-8 flex h-[70px] w-[70px] items-center justify-center rounded-2xl bg-black">
             {icon}

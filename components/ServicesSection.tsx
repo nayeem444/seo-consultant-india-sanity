77e4068d464aff51fd9e7728ed8a56f9a5b1bb06
Nayeem Ahmad Dar <nayeem.marketinglad@gmail.com>
2024-03-20 16:56:38 +0530

    fix

--- components/ServicesSection.tsx
+++ components/ServicesSection.tsx
@@ -151,7 +151,7 @@ const ServiceCard = ({ icon, title, details }) => {
   return (
     <>
       <div className="w-full h-50 px-4 md:w-1/2 lg:w-1/3">
-        <div className="mb-9 rounded-[20px] bg-white p-10 shadow-2 hover:shadow-lg dark:bg-dark-2 md:px-7 xl:px-10">
+        <div className="mb-9 rounded-[20px] bg-white p-4 shadow-2 hover:shadow-lg dark:bg-dark-2 md:px-7 xl:px-10">
           <div className="mb-8 flex h-[70px] w-[70px] items-center justify-center rounded-2xl bg-black">
             {icon}
           </div>

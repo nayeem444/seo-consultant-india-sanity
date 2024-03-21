d33376094c2f2368e3e72c849f6eda7a67865aa2
Nayeem Ahmad Dar <nayeem.marketinglad@gmail.com>
2024-03-21 11:14:16 +0530

    updated

--- components/Headline.tsx
+++ components/Headline.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Headline = () => {
   return (
     <div className='lg:m-24  '>
-<h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl p-6 leading-normal">
+<h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl p-6 leading-normal">
     <span className="block">
     Explore my content which achieves high 
         <span className="text-transparent bg-clip-text bg-gradient-to-tr to-blue-600 ml-4 from-blue-600">
@@ -11,7 +11,7 @@ const Headline = () => {
         </span>
       
     </span>
-</h1>
+</h2>
 
     </div>
   )

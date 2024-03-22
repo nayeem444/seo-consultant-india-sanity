2b291ccd18550bef043169e41425ba33f91726ab
Nayeem Ahmad Dar <nayeem.marketinglad@gmail.com>
2024-03-22 12:19:20 +0530

    font updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,38 @@
-import 'tailwindcss/tailwind.css'
-
-import { VisualEditing } from '@sanity/visual-editing/next-pages-router'
-import { AppProps } from 'next/app'
-import dynamic from 'next/dynamic'
+import 'tailwindcss/tailwind.css';
+import { VisualEditing } from '@sanity/visual-editing/next-pages-router';
+import { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
 import '../styles/globals.css';
 
+// Import the Montserrat font
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({ subsets: ['latin'] });
+
 export interface SharedPageProps {
-  draftMode: boolean
-  token: string
+  draftMode: boolean;
+  token: string;
 }
 
-const PreviewProvider = dynamic(() => import('components/PreviewProvider'))
+const PreviewProvider = dynamic(() => import('components/PreviewProvider'));
 
-export default function App({
-  Component,
-  pageProps,
-}: AppProps<SharedPageProps>) {
-  const { draftMode, token } = pageProps
+export default function App({ Component, pageProps }: AppProps<SharedPageProps>) {
+  const { draftMode, token } = pageProps;
+
   return (
     <>
       {draftMode ? (
         <PreviewProvider token={token}>
-          <Component {...pageProps} />
+          <main className={montserrat.className}>
+            <Component {...pageProps} />
+          </main>
         </PreviewProvider>
       ) : (
-        <Component {...pageProps} />
+        <main className={montserrat.className}>
+          <Component {...pageProps} />
+        </main>
       )}
       {draftMode && <VisualEditing />}
     </>
-  )
+  );
 }

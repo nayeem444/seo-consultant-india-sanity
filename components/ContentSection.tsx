26b6f7365b246208308a1e1a8c94238c22c0898f
Nayeem Ahmad Dar <nayeem.marketinglad@gmail.com>
2024-03-22 14:18:30 +0530

    new updated

--- components/ContentSection.tsx
+++ components/ContentSection.tsx
@@ -5,7 +5,7 @@ import img1 from '../public/Images/WhatsApp Image 2024-02-14 at 3.19.00 PM.jpeg'
 
 const content = () => {
   return (
-    <section className="font-montserrat bg-blueGray-200 -mt-12 mb-48">
+    <section className="bg-blueGray-200 -mt-12 mb-48">
   <div className="container mx-auto px-4">
     <div className="flex flex-wrap">
       <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
@@ -21,10 +21,10 @@ const content = () => {
         <h3 className="text-3xl mb-2 font-semibold leading-normal">
         Trusted SEO Consultant in India
         </h3>
-        <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-blueGray-600">
+        <p className="text-lg  leading-relaxed mt-4 mb-4 text-blueGray-600">
         As an SEO specialist, I understand that it can be difficult to trust someone on the internet claiming to be an expert. However, I have a proven track record of helping businesses develop successful digital strategies that increase rankings, drive quality traffic, and generate revenue.
         </p>
-        <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-blueGray-600">
+        <p className="text-lg  leading-relaxed mt-0 mb-4 text-blueGray-600">
         My understanding of evolving search intent and data-driven approach helps me avoid penalties from core updates. I actively participate in industry events, webinars, and podcasts to share my knowledge and stay up-to-date on the latest SEO techniques and insights.
         </p>
         <div className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" href="/#contact">

fb903e446fcfb3c14dab14b15d095bc4f3919707
Nayeem Ahmad Dar <nayeem.marketinglad@gmail.com>
2024-04-12 18:22:47 +0530

    fix

--- components/caseStudy.tsx
+++ components/caseStudy.tsx
@@ -39,7 +39,7 @@ const caseStudy = () => {
 </div>
 
 <div className=" mb-24 max-w-sm bg-white border border-gray-200 rounded-lg shadow  dark:border-gray-700 mx-4">
-    <a href="case-study/leadgenapp">
+    <a href="/case-study/leadgen">
         <div className="rounded h-56">
             <Image
             className='rounded-lg'
@@ -50,11 +50,11 @@ const caseStudy = () => {
             </div>
     </a>
     <div className="p-5">
-        <a href="case-study/leadgenapp">
+        <a href="/case-study/leadgen">
             <h5 className="mb-2 text-2xl font-bold ">Leadgen App</h5>
         </a>
         <p className="mb-3 font-normal ">Increasing LeadGenApp's Search Impressions By 834% Overall and DR to 69</p>
-        <a href="case-study/leadgenapp" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <a href="/case-study/leadgen" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Read more
              <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>

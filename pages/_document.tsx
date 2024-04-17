2fbff43e58b57cfa6b91c17ab9e13cfe0502560d
Nayeem Ahmad Dar <nayeem.marketinglad@gmail.com>
2024-04-17 20:36:51 +0530

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+     <Head>
+        <link rel="canonical" href="https://shahidshahmiri.com/" />
+      </Head>
       <body className="bg-white text-black">
         <Main />
         <NextScript />

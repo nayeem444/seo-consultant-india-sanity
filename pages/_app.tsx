79e6c8f7e8db44b3e334db0a72a805b740d2ee0f
Nayeem Ahmad Dar <nayeem.marketinglad@gmail.com>
2024-04-17 21:01:54 +0530

    updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,53 @@
+// Import global styles and libraries
 import 'tailwindcss/tailwind.css';
-import { VisualEditing } from '@sanity/visual-editing/next-pages-router';
-import { AppProps } from 'next/app';
-import dynamic from 'next/dynamic';
 import '../styles/globals.css';
 
-// Import the Montserrat font
+// Next.js and other imports
+import { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+
+// Import the Montserrat font using the new Next.js font handling
 import { Montserrat } from 'next/font/google';
 
+// Initialize the Montserrat font
 const montserrat = Montserrat({ subsets: ['latin'] });
 
+// Types for enhanced page properties
 export interface SharedPageProps {
   draftMode: boolean;
   token: string;
 }
 
+// Dynamically import the PreviewProvider component
 const PreviewProvider = dynamic(() => import('components/PreviewProvider'));
 
-export default function App({ Component, pageProps }: AppProps<SharedPageProps>) {
+// Visual editing tools integrated conditionally
+import { VisualEditing } from '@sanity/visual-editing/next-pages-router';
+
+function App({ Component, pageProps }: AppProps<SharedPageProps>) {
   const { draftMode, token } = pageProps;
+  const router = useRouter();
+
+  // Construct canonical URL dynamically based on the router
+  const canonicalUrl = `https://shahidshahmiri.com${router.asPath}`;
+
+  // Extract the slug and format it to generate a title
+  const formatTitle = (slug: string) => {
+    const titleParts = slug.split('/').filter(part => part); // filter to remove empty strings
+    return titleParts.map(part => part.charAt(0).toUpperCase() + part.slice(1)).join(' - ');
+  };
+  
+  // Default title if no slug segments
+  const pageTitle = formatTitle(router.asPath) || 'Home Page';
 
   return (
     <>
+      <Head>
+        <link rel="canonical" href={canonicalUrl} />
+        <title>{pageTitle}</title>
+      </Head>
       {draftMode ? (
         <PreviewProvider token={token}>
           <main className={montserrat.className}>
@@ -36,3 +63,5 @@ export default function App({ Component, pageProps }: AppProps<SharedPageProps>)
     </>
   );
 }
+
+export default App;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-     
+      <Head />
       <body className="bg-white text-black">
         <Main />
         <NextScript />

7d280d77bcd61e00b9363a693089572f1f547dd0
Nayeem Ahmad Dar <nayeem.marketinglad@gmail.com>
2024-04-23 17:32:32 +0530

    bug fix

--- components/PostBody.tsx
+++ components/PostBody.tsx
@@ -38,15 +38,15 @@ export default function PostBody({ content }) {
       <div className="mx-4 md:mx-0 mt-4 md:mt-0 md:w-72"> {/* Apply margin and width */}
         <div className="rounded h-96 bg-blue-600 p-6 flex flex-col justify-center items-center"> {/* Side content */}
           <h1 className="text-white text-3xl font-bold mb-4 text-center">Let Us Handle Your SEO and Marketing</h1>
-          <button 
-            type="button" 
-            className="text-black bg-white hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-white-600 dark:hover:bg-white-700 dark:focus:ring-white-800"
+          <a 
+           href="/contact"
+            className="text-black bg-white hover:bg-white-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-white-600 dark:hover:bg-white-700 dark:focus:ring-white-800"
           >
             Book A Free Call
             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
             </svg>
-          </button>
+          </a>
         </div>
       </div>
     </div>

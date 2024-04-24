dfada1e6cfd4923c56e7480c2155b16f58b2bd6b
Nayeem Ahmad Dar <nayeem.marketinglad@gmail.com>
2024-04-24 21:43:13 +0530

    new

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -21,10 +21,9 @@ const footer = () => {
 <p className=" transition-colors duration-300 dark:text-gray-300  ">Srinagar, India Office
 H NO 3. Qamarwari 2nd Petrol Pump, Near City Mart 190017.</p> 
 </div> </div> </div> <hr className="my-6 border-gray-200 md:my-8 dark:border-gray-700"/>
-<div className="flex items-center justify-between text-white"> 
-© 2024 Shahid Shahmiri. All rights reserved.
- <a href="#"> <img className="w-auto h-7" src="../public/C.png" alt=""/> 
- </a> <div className="flex -mx-2"> <a href="https://twitter.com/shahid_shahmiri" className="transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400" aria-label="twitter"> 
+<div className="flex justify-between -mx-2"> 
+<div className='flex lg:flex-row' >
+<a href="https://twitter.com/shahid_shahmiri" className="transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400" aria-label="twitter"> 
  <svg
   xmlns="http://www.w3.org/2000/svg"
   className="mx-auto h-full w-4"
@@ -42,8 +41,13 @@ H NO 3. Qamarwari 2nd Petrol Pump, Near City Mart 190017.</p>
   <path
     d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
 </svg> </a> 
- </div>
+</div>
+<div className="flex items-center justify-end text-white"> 
+© 2024 Shahid Shahmiri. All rights reserved.
+
   </div> 
+ </div>
+
   </div> 
   </footer>
   )

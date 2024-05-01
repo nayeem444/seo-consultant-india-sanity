fa811f4b4afeb115363636065708bb6b37774c2b
Nayeem Ahmad Dar <nayeem.marketinglad@gmail.com>
2024-05-01 14:53:38 +0530

    new change

--- pages/case-study/index.tsx
+++ pages/case-study/index.tsx
@@ -76,6 +76,7 @@ const index=  ({description , title}) => {
             
             <Navbar/>
             <section className="py-28">
+              <h1 className="hidden">{title}</h1>
                 <div className="max-w-screen-xl mx-auto text-gray-600 gap-x-12 items-center justify-between overflow-hidden md:flex md:px-8">
                     <div className="flex-none space-y-5 px-4 sm:max-w-lg md:px-0 lg:max-w-xl">
                         <h2 className="text-sm text-blue-600 font-medium">

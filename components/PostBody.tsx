9fa93f157647872f42dff7cf7c9cf8c4d03d7f4b
Nayeem Ahmad Dar <nayeem.marketinglad@gmail.com>
2024-05-03 15:01:23 +0530

    updated

--- components/PostBody.tsx
+++ components/PostBody.tsx
@@ -1,27 +1,29 @@
-import {
-  PortableText,
-  type PortableTextReactComponents,
-} from '@portabletext/react'
+import { PortableText, type PortableTextReactComponents } from '@portabletext/react';
+import urlBuilder from '@sanity/image-url'
+import {getImageDimensions} from '@sanity/asset-utils'
+import styles from './PostBody.module.css';
+import { SanityImage } from './SanityImage';
+import Image from 'next/image';
+import img from '../public/Screenshot 2023-02-17 at 5.webp';
 
-import styles from './PostBody.module.css'
-import { SanityImage } from './SanityImage'
-import Image from 'next/image'
-import img from '../public/Screenshot 2023-02-17 at 5.webp' 
-
-const serializers = {
+const myPortableTextComponents: PortableTextReactComponents = {
+  types: {
+    image: ({ value }) => {
+      const { width, height } = value;
+      return <SanityImage {...value} width={width} height={height} />;
+    },
+  },
   marks: {
-    link: ({ children, mark }) => {
-      const relAttributes = [];
-      if (mark.blank) {
-        relAttributes.push('noopener noreferrer');
-      }
-      if (mark.nofollow) {
-        relAttributes.push('nofollow');
-      }
-      const relAttribute = relAttributes.length > 0 ? relAttributes.join(' ') : null;
+    link: ({ children, value }) => {
+      const { href, blank, nofollow } = value;
+      const target = blank ? '_blank' : '_self';
+      const rel = [
+        !href.startsWith('/') ? 'noreferrer noopener' : '',
+        nofollow ? 'nofollow' : '',
+      ].filter(Boolean).join(' ');
 
       return (
-        <a href={mark.href} target={mark.blank ? '_blank' : '_self'} rel={relAttribute}>
+        <a href={href} target={target} rel={rel}>
           {children}
         </a>
       );
@@ -29,48 +31,63 @@ const serializers = {
   },
 };
 
-const myPortableTextComponents: Partial<PortableTextReactComponents> = {
-  types: {
-    image: ({ value }) => {
-      return <SanityImage {...value} />
-    },
-  },
-};
-
 export default function PostBody({ content }) {
   return (
-    <div className="flex flex-col md:flex-row"> {/* Flex container */}
+    <div className="flex flex-col md:flex-row">
+      {/* Flex container */}
       {/* Left column (main content) */}
-      <div className="mx-4 md:mx-24 max-w-full md:max-w-4xl mb-8 md:mb-0"> {/* Apply margin and max width */}
-        <div className={`${styles.portableText}`}> {/* Content */}
-          <PortableText value={content} components={myPortableTextComponents} serializers={serializers} />
+      <div className="mx-4 md:mx-24 max-w-full md:max-w-4xl mb-8 md:mb-0">
+        {/* Apply margin and max width */}
+        <div className={`${styles.portableText}`}>
+          {/* Content */}
+          <PortableText value={content} components={myPortableTextComponents} />
         </div>
       </div>
-      
       {/* Right column (side content) */}
-      <div className="mx-4 md:mx-0 mt-4 md:mt-0 md:w-72"> {/* Apply margin and width */}
-        <div className="sticky top-4 rounded h-96 bg-blue-600 p-6 flex flex-col justify-center items-center"> {/* Side content */}
+      <div className="mx-4 md:mx-0 mt-4 md:mt-0 md:w-72">
+        {/* Apply margin and width */}
+        <div className="sticky top-4 rounded h-96 bg-blue-600 p-6 flex flex-col justify-center items-center">
+          {/* Side content */}
           <h2 className="text-white text-3xl font-bold mb-4 text-center">Want to see a similar trend in your GSC ?</h2>
-          <div className='py-4'>
-            <Image
-              width={400}
-              height={200}
-              src={img}
-              className='rounded'
-            />
+          <div className="py-4">
+            <Image width={400} height={200} src={img} className="rounded" />
           </div>
-        
-          <a 
+          <a
             href="/contact"
             className="text-black bg-white hover:bg-white-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-white-600 dark:hover:bg-white-700 dark:focus:ring-white-800"
           >
             Book A Free Call
             <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
             </svg>
           </a>
         </div>
       </div>
     </div>
   );
+}
+
+
+
+const SampleImageComponent = ({value, isInline}) => {
+  const {width, height} = getImageDimensions(value)
+  return (
+    <img
+      src={urlBuilder()
+        .image(value)
+        .width(isInline ? 100 : 800)
+        .fit('max')
+        .auto('format')
+        .url()}
+      alt={value.alt || ' '}
+      loading="lazy"
+      style={{
+        // Display alongside text if image appears inside a block text span
+        display: isInline ? 'inline-block' : 'block',
+
+        // Avoid jumping around with aspect-ratio CSS property
+        aspectRatio: width / height,
+      }}
+    />
+  )
 }
--- lib/sanity.queries.ts
+++ lib/sanity.queries.ts
@@ -1,4 +1,4 @@
-import { groq } from 'next-sanity'
+import { groq } from 'next-sanity';
 
 const postFields = groq`
   _id,
@@ -9,58 +9,80 @@ const postFields = groq`
   coverImage,
   "slug": slug.current,
   "author": author->{name, picture},
-`
+  content[] {
+    ...,
+    markDefs[] {
+      ...,
+      _type == "link" => {
+        ...,
+        nofollow,
+        blank
+      }
+    }
+  }
+`;
 
-export const settingsQuery = groq`*[_type == "settings"][0]`
+export const settingsQuery = groq`*[_type == "settings"][0]`;
 
 export const indexQuery = groq`
-*[_type == "post"] | order(date desc, _updatedAt desc) {
-  ${postFields}
-}`
-
-export const postAndMoreStoriesQuery = groq`
-{
-  "post": *[_type == "post" && slug.current == $slug] | order(_updatedAt desc) [0] {
-    content,
-    ${postFields}
-  },
-  "morePosts": *[_type == "post" && slug.current != $slug] | order(date desc, _updatedAt desc) [0...2] {
-    content,
+  *[_type == "post"] | order(date desc, _updatedAt desc) {
     ${postFields}
   }
-}`
+`;
+
+export const postAndMoreStoriesQuery = groq`
+  {
+    "post": *[_type == "post" && slug.current == $slug] | order(_updatedAt desc) [0] {
+      ${postFields}
+    },
+    "morePosts": *[_type == "post" && slug.current != $slug] | order(date desc, _updatedAt desc) [0...2] {
+      ${postFields}
+    }
+  }
+`;
 
 export const postSlugsQuery = groq`
-*[_type == "post" && defined(slug.current)][].slug.current
-`
+  *[_type == "post" && defined(slug.current)][]._id
+`;
 
 export const postBySlugQuery = groq`
-*[_type == "post" && slug.current == $slug][0] {
-  ${postFields}
-}
-`
+  *[_type == "post" && slug.current == $slug][0] {
+    ${postFields}
+  }
+`;
 
 export interface Author {
-  name?: string
-  picture?: any
+  name?: string;
+  picture?: any;
+}
+
+export interface Link {
+  _type: 'link';
+  href?: string;
+  nofollow?: boolean;
+  blank?: boolean;
 }
 
 export interface Post {
-  _id: string
-  title?: string
-  coverImage?: any
-  date?: string
-  _updatedAt?: string
-  excerpt?: string
-  author?: Author
-  slug?: string
-  content?: any
+  _id: string;
+  title?: string;
+  coverImage?: any;
+  date?: string;
+  _updatedAt?: string;
+  excerpt?: string;
+  author?: Author;
+  slug?: string;
+  content?: Array<{
+    _type: string;
+    markDefs?: Link[];
+    [key: string]: any;
+  }>;
 }
 
 export interface Settings {
-  title?: string
-  description?: any[]
+  title?: string;
+  description?: any[];
   ogImage?: {
-    title?: string
-  }
+    title?: string;
+  };
 }

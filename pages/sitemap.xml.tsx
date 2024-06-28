05feb2c7b142c648137a1c8ba36f6ba0589cfa9c
Nayeem Ahmad Dar <Nayeemdar3@gmail.com>
2024-06-28 16:08:38 +0530

    fix

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,8 +1,6 @@
-const EXTERNAL_DATA_URL = 'http://marketinglad.co.in/wp-json/wp/v2/posts';
+const baseUrl = 'https://shahidshahmiri.com';
 
-function generateSiteMap(posts) {
-  const baseUrl = 'https://shahidshahmiri.com';
-
+function generateSiteMap() {
   return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
@@ -26,24 +24,11 @@ function generateSiteMap(posts) {
   <url>
     <loc>${baseUrl}/case-study/apexure</loc>
   </url>
-  ${posts
-    .map(({ id, slug, modified }) => {
-      return `
-        <url>
-          <loc>${baseUrl}/blog/${slug}</loc>
-          <lastmod>${new Date(modified).toISOString()}</lastmod>
-        </url>
-      `;
-    })
-    .join('')}
 </urlset>`;
 }
 
 export async function getServerSideProps({ res }) {
-  const request = await fetch(EXTERNAL_DATA_URL);
-  const posts = await request.json();
-
-  const sitemap = generateSiteMap(posts);
+  const sitemap = generateSiteMap();
 
   res.setHeader('Content-Type', 'text/xml');
   res.write(sitemap);

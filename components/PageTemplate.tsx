3f422aa2b285d954cfcde0f9390d51e958ada2bd
Nayeem Ahmad Dar <Nayeemdar3@gmail.com>
2024-09-02 14:28:16 +0530

    mobile reponsiveness added

--- components/PageTemplate.tsx
+++ components/PageTemplate.tsx
@@ -1,40 +1,33 @@
-
-
 import React from 'react';
 import { PageContent } from '../types/content';
 import { decodeHtml } from '../utils/htmlDecoder';
 import Navbar from './Navbar2';
 import Footer2 from './Footer2';
 
-
 interface PageTemplateProps {
   content: PageContent;
 }
 
 const PageTemplate: React.FC<PageTemplateProps> = ({ content }) => (
-  <div>
-    <Navbar/>
-    <main className=" px-[14rem] pt-[8rem] pb-4" >
+  <div className="flex flex-col min-h-screen">
+    <Navbar />
+    <main className="flex-grow px-4 pt-20 pb-4 sm:px-8 md:px-16 lg:px-32 xl:px-56">
       {/* <h1 className="text-3xl font-bold mb-6">{content.title}</h1> */}
-    
-
 
       {content.questions.map((qa, index) => (
-          <div key={index} className="mb-8">
-            <h2 className="text-2xl font-semibold mb-4">{decodeHtml(qa.question)}</h2>
-            {Array.isArray(qa.answer) ? (
-              qa.answer.map((paragraph, pIndex) => (
-                <p key={pIndex} className="mb-4">{decodeHtml(paragraph)}</p>
-              ))
-            ) : (
-              <p className="mb-4">{decodeHtml(qa.answer)}</p>
-            )}
-          </div>
-        ))}
-
+        <div key={index} className="mb-8">
+          <h2 className="text-xl font-semibold mb-4 md:text-2xl">{decodeHtml(qa.question)}</h2>
+          {Array.isArray(qa.answer) ? (
+            qa.answer.map((paragraph, pIndex) => (
+              <p key={pIndex} className="mb-4 text-sm sm:text-base">{decodeHtml(paragraph)}</p>
+            ))
+          ) : (
+            <p className="mb-4 text-sm sm:text-base">{decodeHtml(qa.answer)}</p>
+          )}
+        </div>
+      ))}
 
       {/* <h2 className="text-xl font-semibold mb-2">Check out this video to know more:</h2> */}
-   
     </main>
     {/* <aside className="w-1/4">
       <h2 className="text-xl font-semibold mb-4">Recent Heading</h2>
@@ -44,7 +37,7 @@ const PageTemplate: React.FC<PageTemplateProps> = ({ content }) => (
         ))}
       </ul>
     </aside> */}
-    <Footer2/>
+    <Footer2 />
   </div>
 );
 

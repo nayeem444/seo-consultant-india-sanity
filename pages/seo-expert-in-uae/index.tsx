3243686e33499e829bf19f74cd5ea615882a1bc2
Nayeem Ahmad Dar <Nayeemdar3@gmail.com>
2024-09-24 19:57:31 +0530

    new fix 2

--- pages/seo-expert-in-uae/index.tsx
+++ pages/seo-expert-in-uae/index.tsx
@@ -1,8 +1,5 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/react";
+
 import Head from 'next/head';
-import Link from "next/link";
-import Script from 'next/script'; // Import Next.js Script component
 import Calan from "../../components/PopUp";
 import Hero2 from "./Hero3";
 import ExitPopup from "components/Banner";
@@ -18,67 +15,64 @@ import CaseStudies from "components/CaseStudies";
 import FaqComponent from "./FaqComponent";
 import TestimonialsCarousel from "components/TestimonialsCarousel";
 import Footer2 from "components/Footer2";
-import ErrorBoundary from "components/ErrorBoundary";
 
-export default function Home({ description, title }) {
-  return (
-    <>
-      <Head>
-        <Analytics />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-
-        <meta name="description" content={description} />
+export default function Home({discription ,title}) {
+  debugger
+  return ( 
+   <>
+    <Head>
+    
+    <link rel="icon" href="/favicon.ico" sizes="any" />
+           
+        <meta name="description" content=" Looking for the #1 SEO consultant in UAE? My proven tactics drive traffic and increase revenue. Schedule a Free consultation." />
         <link rel="canonical" href="https://shahidshahmiri.com/" />
-        <Link rel="alternate" hrefLang="en-ae" href="https://shahidshahmiri.com/seo-expert-in-uae" />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "ProfessionalService",
+              "name": "Shahid Shahmiri SEO Expert Consultant in India",
+              "url": "https://shahidshahmiri.com/",
+              "address": {
+                "@type": "PostalAddress",
+                "addressCountry": "AE",
+                "addressLocality": "Dubai",
+                "addressRegion": "Dubai"
+              },
+              "areaServed": "AE"
+            })
+          }}
+        />
       </Head>
 
-      {/* Use Next.js Script component to inject JSON-LD
-      <Script
-        id="json-ld"
-        type="application/ld+json"
-        strategy="afterInteractive" // Ensures script runs after page is interactive
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "ProfessionalService",
-            "name": "Shahid Shahmiri SEO Expert in UAE",
-            "url": "https://shahidshahmiri.com/seo-expert-in-uae",
-            "address": {
-              "@type": "PostalAddress",
-              "addressCountry": "AE",
-              "addressLocality": "Dubai",
-              "addressRegion": "Dubai"
-            },
-            "areaServed": "AE"
-          })
-        }}
-      /> */}
 
-      <SpeedInsights />
 
-      <Navbar2 />
-      <Hero2 />
-      <Clients />
-      <AboutMe />
-      <Explore />
-      <WorkHistory />
-      <SEOExpertise />
-      <Services />
-      <MyBrands />
-      <Calan />
-      <ExitPopup />
-      <CaseStudies />
-      <FaqComponent />
-      <TestimonialsCarousel />
-      <Footer2 />
-    </>
-  );
+<Navbar2/>
+<Hero2/>
+<Clients/>
+<AboutMe/>
+<Explore/>
+<WorkHistory/>
+<SEOExpertise/>
+<Services/>
+<MyBrands/>
+<Calan/>
+<ExitPopup/>
+<CaseStudies/>
+<FaqComponent/>
+<TestimonialsCarousel/>
+<Footer2/>
+   </>
+  )
+
+  
 }
-
 export async function getStaticProps() {
-  const description = "Looking for the #1 SEO consultant in UAE? My proven tactics drive traffic and increase revenue. Schedule a Free consultation.";
-  const title = "Shahid Shahmiri - Your Expert SEO Consultant in UAE";
+  const description = " Looking for the #1 SEO consultant in UAE? My proven tactics drive traffic and increase revenue. Schedule a Free consultation.";
+  const title = "Shahid Shahmiri - Your Expert SEO Consultant in UAE"
   return {
-    props: { description, title }, // Pass the description and title to the component
-  };
-}
+      props: { description,title }, // Pass the description to the component
+    };
+  }
+
